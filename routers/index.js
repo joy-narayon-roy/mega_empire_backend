ee829b272221242f549642194df6554a5a653546
joyroy261216@gmail.com <demoyeti420@gmail.com>
2024-05-11 15:19:58 +0600

    Fix all dependabot

--- routers/index.js
+++ routers/index.js
@@ -6,6 +6,11 @@ const playersRoutes = require("./playersRoutes");
 router.use("/api/v2/clans", clansRoutes);
 router.use("/api/v2/players", playersRoutes);
 router.use("/api/v2/apikey", apikeyRoutes);
+
+router.get("/", (req, res) => {
+  return res.status(200).json({ message: "Looks good." });
+});
+
 router.use("*", (_req, _res, next) => {
   const err = new Error("Path not found");
   err.status = 404;
