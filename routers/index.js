5f179ac9a78d757b075eb31dc4d1a930cc9fb370
joyroy261216@gmail.com <demoyeti420@gmail.com>
2024-05-11 11:38:35 +0600

    Global 404 error handeler set.

--- routers/index.js
+++ routers/index.js
@@ -3,5 +3,10 @@ const clansRoutes = require("./clansRoutes");
 const apikeyRoutes = require("./apikeyRoutes");
 router.use("/api/v2/clans", clansRoutes);
 router.use("/api/v2/apikey", apikeyRoutes);
+router.use("*", (req, res, next) => {
+  const err = new Error("Path not found");
+  err.status = 404;
+  next(err);
+});
 
 module.exports = router;
